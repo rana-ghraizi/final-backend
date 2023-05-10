ff16d97eefa5517afdf162d790ba12e84d8a7822
Rana Ghraizi <ranaghraizi1@gmail.com>
2023-05-10 22:15:17 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+
+dotenv.config();
+
+await connectDB();
+
+const PORT = process.env.PORT || 5000;
+const app = new express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+app.listen(
+  PORT,
+  console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}!!!`)
+);
